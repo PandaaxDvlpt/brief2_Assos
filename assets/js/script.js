9431109c84558d82f14432c2f0fa03a03c1192c6
PandaaxY <pandaaxytb@gmail.com>
2025-04-14 10:49:16 +0200

    Page Fed Terminée

---
+++ assets/js/script.js
@@ -0,0 +1,24 @@
+// Boutons Cliquables
+// ----------------------------------------------------------------------------------
+document.querySelector('.logo').addEventListener('click', function() {
+    window.location.href = '../index.html'; 
+});
+document.querySelector('.youtube-button').addEventListener('click', function() { 
+    window.location.href = 'https://youtube.com/@TheCodingTrain'; 
+});
+document.querySelector('.instagram-button').addEventListener('click', function() { 
+    window.location.href = 'https://youtube.com/@TheCodingTrain'; 
+});
+document.querySelector('.facebook-button').addEventListener('click', function() { 
+    window.location.href = 'https://www.facebook.com/federationfrancaisedeforce/?locale=fr_FR'; 
+});
+document.querySelector('.sbd').addEventListener('click', function() { 
+    window.location.href = 'https://sbd.fr/'; 
+});
+document.querySelector('.sw').addEventListener('click', function() { 
+    window.location.href = 'https://silentworker.fr/en/'; 
+});
+document.querySelector('.maniak').addEventListener('click', function() { 
+    window.location.href = 'https://maniakfitness.com/fr-fr/?srsltid=AfmBOopdEx5RFdi9U7JoDAyYgKZj1jRDHVVUpZKq5Id9JS-mbzt68clE'; 
+});
+// ----------------------------------------------------------------------------------
