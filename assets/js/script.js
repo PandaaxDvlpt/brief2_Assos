de25ea447ec61cb038fffb47815f72662e86b22e
PandaaxY <pandaaxytb@gmail.com>
2025-04-17 15:41:24 +0200

    Dark Button

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,3 +22,31 @@ document.querySelector('.maniak').addEventListener('click', function() {
     window.location.href = 'https://maniakfitness.com/fr-fr/?srsltid=AfmBOopdEx5RFdi9U7JoDAyYgKZj1jRDHVVUpZKq5Id9JS-mbzt68clE'; 
 });
 // ----------------------------------------------------------------------------------
+
+// Dark Mod 
+
+// ----------------------------------------------------------------------------------
+const toggleBtn = document.getElementById('toggle-btn');
+const body = document.querySelector('body');
+
+let darkMod = localStorage.getItem('dark-mode');
+
+const setDarkMode = (enable) => {
+    if (enable) {
+        body.classList.toggle('dark-mode-theme');
+        toggleBtn.classList.toggle('dark-mode-toggle');
+        localStorage.setItem('dark-mode', 'enabled');
+    } else {
+        body.classList.remove('dark-mode-theme');
+        toggleBtn.classList.remove('dark-mode-toggle');
+        localStorage.setItem('dark-mode', 'disabled');
+    }
+};
+toggleBtn.onclick = function() {
+    darkMod = localStorage.getItem("dark-mode");
+        if(darkMod === 'disabled') {
+            setDarkMode(true);
+        } else {
+            setDarkMode(false);
+        }
+}
